088fd768045c34350744d31e3a99a62a39d5d64c
Maiosama02 <mai.karoom02@gmail.com>
2023-11-01 03:26:46 +0200

    p

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pthon3
+def pow(a, b)
+    result = 1
+    for 1 in range(b):
+        result *= a
+    return result
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     lastdigit = abs(number) % 10
     if number < 0:
-       lastdigit = number % -10
-       lastdigit *= -1
+        lastdigit = number % -10
+        lastdigit *= -1
     print("{:d}".format(lastdigit), end='')
     return lastdigit
