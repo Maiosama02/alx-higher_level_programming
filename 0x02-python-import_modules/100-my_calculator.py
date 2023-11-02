410333d071e66b7625479cd160e0e009f98764f7
Maiosama02 <mai.karoom02@gmail.com>
2023-11-02 18:58:25 +0200

    py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    args = sys.argv
+    if len(args) < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit (1)
+    if not (args[2] == "+" or args[2] == "-" or args[2] == "*" or args[2] == "/"):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit (1)
+    if args[2] == "+":
+        print("{} + {} = {}".format(args[1], args[3], add(int(args[1]), int(args[3]))))
+    elif args[2] == "-":
+        print("{} - {} = {}".format(args[1], args[3], sub(int(args[1]), int(args[3]))))
+    elif args[2] == "*":
+        print("{} * {} = {}".format(args[1], args[3], mul(int(args[1]), int(args[3]))))
+    elif args[2] == "/":
+        print("{} / {} = {}".format(args[1], args[3], div(int(args[1]), int(args[3]))))
