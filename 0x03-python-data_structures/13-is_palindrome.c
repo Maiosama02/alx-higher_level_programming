69822473a9d6af7035e63b8e4c3598a51e97fc47
Maiosama02 <mai.karoom02@gmail.com>
2023-11-06 15:13:01 +0200

    data struct

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,90 @@
+#include "lists.h"
+
+/**
+ * reverse_list = fuction that reverse a list
+ * @head: head pointer
+ * Return: pointer
+*/
+
+listint_t *reverse_list(listint_t *head)
+{
+    listint_t *prev = NULL;
+    listint_t *curr = head;
+    listint_t *next;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+/**
+ * compare_lists - compare 2 lists
+ * @head1: first pointer
+ * @head2: second pointer
+ * Return: 1 if equal, 0 otherwise
+*/
+
+int compare_lists(listint_t *head1, listint_t *head2)
+{
+    listint_t *temp1 = head1;
+    listint_t *temp2 = head2;
+
+    while (temp1 && temp2)
+    {
+        if (temp1->n == temp2->n)
+        {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        else
+            return 0;
+    }
+
+    if (temp1 == NULL && temp2 == NULL)
+        return 1;
+
+    return 0;
+}
+
+/**
+ * is_palindrome - check if a list is palindrome
+ * @head: pointer
+ * Return: result
+*/
+int is_palindrome(listint_t **head)
+{
+    listint_t *slow, *fast, *second_half, *prev_of_slow;
+    int res;
+
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
+
+    slow = fast = *head;
+    prev_of_slow = NULL;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        prev_of_slow = slow;
+        slow = slow->next;
+    }
+
+    if (fast != NULL)
+        slow = slow->next;
+
+    second_half = reverse_list(slow);
+
+    res = compare_lists(*head, second_half);
+
+    second_half = reverse_list(second_half);
+
+    if (prev_of_slow != NULL)
+        prev_of_slow->next = second_half;
+
+    return res;
+}
